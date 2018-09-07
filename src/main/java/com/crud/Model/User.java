4214b07b3989288f6f9351d3eb35d347de245f36
kishore dabi <kishoredabi@email.com>
2018-09-07 19:53:32 +0530

    user model changes

--- src/main/java/com/crud/Model/User.java
+++ src/main/java/com/crud/Model/User.java
@@ -1,8 +1,22 @@
 package com.crud.Model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
 public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id")
 	private String id;
+	@Column(name = "name")
 	private String name;
+	@Column(name = "desc")
 	private String description;
 
 	public User(String id, String name, String description) {
@@ -28,5 +42,9 @@ public class User {
 	}
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", description=" + description + "]";
 	}
 }
